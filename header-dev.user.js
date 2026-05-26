355e0e7cac9e3515641ea2da58476ec50f874a69
schmitzr <schmitzr@essilor.de>
2026-05-26 20:32:13 +0200

    bump version to 1.0.4

--- header-dev.user.js
+++ header-dev.user.js
@@ -2,7 +2,7 @@
 // @name         Outlook Web - Shipment Tracking Linker (DEV)
 // @namespace    github.com/ruffy314/
 // @author       Ruffy314
-// @version      1.0.0.3
+// @version      1.0.0.7
 // @description  Turn tracking numbers into links in Outlook Web
 // @match        https://outlook.office.com/*
 // @match        https://outlook.cloud.microsoft/*
